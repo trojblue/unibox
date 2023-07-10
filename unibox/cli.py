cd324bdad77ae65c15b2ea99884607162ba2e0cf
yada <trojblue@gmail.com>
2023-07-10 18:23:25 -0400

    feat: updating cli & click requirement version

--- unibox/cli.py
+++ unibox/cli.py
@@ -16,13 +16,25 @@ def awscli():
     print('Setting up awscli')
 
 @cli.command()
-@click.argument('dir_name')
-def resize(src_dir):
+@click.argument('src_dir')
+@click.option('--min_side', '-m', default=None, type=int, help='Minimum side length of the image.')
+@click.option('--dst_dir', '-d', default=None, help='Destination directory for the images.')
+@click.option('--format', '-f', default=None, help='Format of the image.')
+@click.option('--quality', '-q', default=None, type=int, help='Quality of the image.')
+def resize(src_dir, min_side, dst_dir, format, quality):
     # resize implementation
     print(f'Resizing directory: {src_dir}')
-    min_side = 768
-    dst_dir = f"{src_dir}_{min_side}webp"
-    resizer = image_resizer.ImageResizer(src_dir, dst_dir, min_side, format="webp", quality=95)
+
+    if not min_side:
+        min_side = click.prompt('- Minimum side length', type=int, default=768)
+    if not format:
+        format = click.prompt('- File format', default="webp")
+    if not dst_dir:
+        dst_dir = click.prompt('- Destination', default=f"{src_dir}_{min_side}_{format}")
+    if not quality:
+        quality = click.prompt('- Image quality', type=int, default=95)
+
+    resizer = image_resizer.ImageResizer(src_dir, dst_dir, min_side, format=format, quality=quality)
     resizer.resize_images()
 
 cli.add_command(setup)
