8f5b9878f0e9f16b3e1aa24df9a76bfb8ec858fb
yada <trojblue@gmail.com>
2023-07-10 18:12:43 -0400

    tweak: add resizer to cli

--- unibox/cli.py
+++ unibox/cli.py
@@ -1,5 +1,7 @@
 import click
 
+from .processing import image_resizer
+
 @click.group()
 def cli():
     pass
@@ -15,9 +17,13 @@ def awscli():
 
 @cli.command()
 @click.argument('dir_name')
-def resize(dir_name):
+def resize(src_dir):
     # resize implementation
-    print(f'Resizing directory: {dir_name}')
+    print(f'Resizing directory: {src_dir}')
+    min_side = 768
+    dst_dir = f"{src_dir}_{min_side}webp"
+    resizer = image_resizer.ImageResizer(src_dir, dst_dir, min_side, format="webp", quality=95)
+    resizer.resize_images()
 
 cli.add_command(setup)
 
