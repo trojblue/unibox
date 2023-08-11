4eec7b2a885ace831cd52484e6f2bd9096379e7d
yada <trojblue@gmail.com>
2023-08-11 02:05:48 -0400

    feat: adding UniSaver and unibox.saves() method; bump version number to 0.2.0

--- unibox/__init__.py
+++ unibox/__init__.py
@@ -1,9 +1,11 @@
 from __future__ import annotations
 
 from pathlib import Path
+from typing import Any
 
 from .utils.uni_loader import UniLoader
 from .utils.uni_logger import UniLogger
+from .utils.uni_saver import UniSaver
 
 def loads(file_path: Path | str) -> any:
     """
@@ -17,3 +19,23 @@ def loads(file_path: Path | str) -> any:
     """
     Loader = UniLoader()
     return Loader.loads(file_path)
+
+
+def saves(data: Any, file_path: Path | str) -> None:
+    """
+    Saves arbitrary data to the given file path, using the uniSaver.save() method.
+    :param data: The data to save.
+    :param file_path: Path to the file to save.
+
+    example:
+    >>> json_dict = {"name": "John", "age": 30}
+    >>> unibox.saves(json_dict, "data.json")
+    >>> json_dict_list = [{"name": "John", "age": 30}, {"name": "Doe", "age": 40}]
+    >>> unibox.saves(json_dict_list, "data.jsonl")
+    >>> df = pd.DataFrame({'A': [1, 2], 'B': [3, 4]})
+    >>> unibox.saves(df, "data.parquet")
+    >>> pil_image = Image.new('RGB', (60, 30), color='red')
+    >>> unibox.saves(pil_image, "image.png")
+    """
+    saver = UniSaver()
+    saver.save(data, file_path)
