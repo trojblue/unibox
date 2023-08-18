7e3f6dffbcf6e3972dceeaa8d23c4b639d80cbf8
yada <trojblue@gmail.com>
2023-08-18 07:02:44 -0400

    tweak: add get_traversed_fils() for UniTraverser

--- unibox/utils/uni_traverser.py
+++ unibox/utils/uni_traverser.py
@@ -81,6 +81,12 @@ class UniTraverser:
             relative_path = relative_path.replace("\\", "/")
         return relative_path
 
+    def get_traversed_fils(self, relative_unix=False):
+        if relative_unix:
+            return [self.to_relative_unix_path(file_path) for file_path in self.traversed_files]
+        else:
+            return self.traversed_files
+
 
 # example code
 def pre_process():
