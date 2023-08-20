26bb4df61d6cfb0f6f37dbc3b92ef8134c8013c3
yada <trojblue@gmail.com>
2023-08-20 14:41:39 -0400

    feat: reducing minimum  python dependency from 3.10 to 3.8

--- unibox/__init__.py
+++ unibox/__init__.py
@@ -7,7 +7,7 @@ from .utils.uni_loader import UniLoader
 from .utils.uni_logger import UniLogger
 from .utils.uni_saver import UniSaver
 from .utils.uni_traverser import UniTraverser
-from .utils import constants  #  from unibox.constants import IMG_FILES
+from .utils import constants #  from unibox.constants import IMG_FILES
 
 def loads(file_path: Path | str) -> any:
     """
