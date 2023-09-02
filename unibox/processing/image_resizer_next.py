4ba16d9e0e09e53712b22059d30177c45252ef3e
yada <trojblue@gmail.com>
2023-09-02 19:28:24 -0400

    fix: find existing images and remove them from jobs list

--- unibox/processing/image_resizer_next.py
+++ unibox/processing/image_resizer_next.py
@@ -13,6 +13,24 @@ from typing import List, Tuple
 
 
 class ImageResizer:
+    """
+    Resizes images in a directory to a specified size.
+
+    Resolution:
+        - by min_dim: resize the shorter side to a specified size
+        - by max_dim: resize the longer side to a specified size
+        - by target_pixels: resize the shorter side to a specified number of pixels
+
+    Methods:
+        - by directory: resize all images in a directory
+        - by list: resize a list of images given
+
+    Usage:
+    >>> resizer = ImageResizer(root_dir, dst_dir, min_dim=min_dim, max_dim=max_dim, target_pixels=target_pixels,
+    >>>                   keep_hierarchy=False, exist_ok=True)
+    >>> resizer.execute_resize_jobs(resizer.get_resize_jobs())
+    """
+
     TRUNCATE_MULTIPLIER = 32  # the shorter side will be a multiple of 32 after scaling
     WEBP_QUALITY = 98  # quality of the output image (max:100)
     EXTENSION = ".webp"  # file extension (.webp)
@@ -168,18 +186,40 @@ class ImageResizer:
             futures = [executor.submit(task, *args) for task, *args in tasks]
             list(tqdm(as_completed(futures), total=len(tasks), desc="Resizing images"))
 
-    def resizer_sketch(self):
+    def get_resize_jobs(self) -> List[str]:
+        """
+        Get the list of image files that need to be resized.
 
+        :return: List of image paths relative to self.root_dir to be resized
+        """
         self.logger.info("Getting image paths...")
-        image_files = unibox.traverses(self.root_dir, include_extensions=unibox.constants.IMG_FILES, relative_unix=True)
+        todo_image_files = unibox.traverses(self.root_dir, include_extensions=unibox.constants.IMG_FILES,
+                                            relative_unix=True)
 
         if self.exist_ok:
             self.logger.info("Checking existing files...")
             existing_files = unibox.traverses(self.dst_dir, include_extensions=unibox.constants.IMG_FILES,
                                               relative_unix=False)
-            expected_files = [self._get_dst_path(og_rel_image_path) for og_rel_image_path in image_files]
-            image_files = list(set(expected_files) - set(existing_files))
 
+            expected_file_set = {self._get_dst_path(rel_path) for rel_path in todo_image_files}
+            existing_file_set = set(existing_files)
+
+            # Find the difference between the two sets
+            needed_files_set = expected_file_set - existing_file_set
+
+            # Map back to original relative paths
+            reverse_mapping = {self._get_dst_path(rel_path): rel_path for rel_path in todo_image_files}
+            todo_image_files = [reverse_mapping[dst_path] for dst_path in needed_files_set]
+
+        return todo_image_files
+
+    def execute_resize_jobs(self, image_files: List[str]) -> None:
+        """
+        Execute resizing tasks for the given list of image files.
+
+        :param image_files: List of relative image paths to be resized
+            e.g. "root_dir/some_dir/image.jpg" -> entry should be "some_dir/image_resized.jpg"
+        """
         tasks = [(self._resize_single_image_task, og_rel_image_path) for og_rel_image_path in image_files]
 
         self.logger.info("Resizing images...")
@@ -190,10 +230,12 @@ if __name__ == '__main__':
     root_dir = r"E:\_benchmark\1k"
     dst_dir = r"E:\_benchmark\1k_resized"
 
-    min_dim = 512
-    max_dim = int(1024 * 3)
-    target_pixels = int(1024 * 1024 * 1.25)
+    min_dim = 512  # lower priority
+    max_dim = int(1024 * 3)  # higher priority
+    target_pixels = int(1024 * 1024 * 1.25)  # highest priority
 
     resizer = ImageResizer(root_dir, dst_dir, min_dim=min_dim, max_dim=max_dim, target_pixels=target_pixels,
                            keep_hierarchy=False, exist_ok=True)
-    resizer.resizer_sketch()
+
+    images_to_resize = resizer.get_resize_jobs()
+    resizer.execute_resize_jobs(images_to_resize)
