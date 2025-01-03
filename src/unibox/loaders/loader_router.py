7747b47e34d3efeb653124bdd5be9e892d02bce2
yada <trojblue@gmail.com>
2025-01-03 17:31:01 +0000

    feat: adding basic working loader and tests

--- src/unibox/loaders/loader_router.py
+++ src/unibox/loaders/loader_router.py
@@ -2,16 +2,24 @@
 from pathlib import Path
 from typing import Optional
 from .csv_loader import CSVLoader
-from .jsonl_loader import JSONLoader
+from .image_loder import ImageLoader
+from .json_loader import JSONLoader
+from .jsonl_loader import JSONLLoader
 from .parquet_loader import ParquetLoader
 # ... etc
+
+from ..utils.constants import IMG_FILES
 
 def get_loader_for_suffix(suffix: str):
     suffix = suffix.lower()
     if suffix == ".csv":
         return CSVLoader()
+    elif suffix in IMG_FILES:
+        return ImageLoader()
     elif suffix == ".json":
         return JSONLoader()
+    elif suffix == ".jsonl":
+        return JSONLLoader()
     elif suffix == ".parquet":
         return ParquetLoader()
     # ...
