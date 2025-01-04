3914819723b1e17d71311627d0b5cfb1b94f7980
yada <trojblue@gmail.com>
2025-01-04 08:52:11 +0000

    tweak: adding merge dict util function

--- src/unibox/utils/utils.py
+++ src/unibox/utils/utils.py
@@ -1,6 +1,8 @@
 import os
 from urllib.parse import urlparse
 
+import logging
+logger = logging.getLogger(__name__)
 
 def to_relaive_unix_path(absolute_path: str, root_dir: str, convert_slash=True):
     relative_path = os.path.relpath(absolute_path, root_dir)
@@ -26,3 +28,30 @@ def is_url(path: str) -> bool:
 def is_hf_uri(uri: str) -> bool:
     """Check if the URI is a Hugging Face URI."""
     return uri.startswith("hf://")
+
+
+def merge_dicts(*dicts):
+    """
+    Merge dictionaries, raising warnings for overlapping keys and data type mismatches.
+
+    Args:
+        *dicts: Dictionaries to merge.
+        logger: Logger instance for warnings (default: None).
+
+    Returns:
+        dict: Merged dictionary.
+    """
+    assert all(isinstance(d, dict) for d in dicts), "All inputs must be dictionaries."
+
+    result = {}
+    for d in dicts:
+        for key, value in d.items():
+            if key in result:
+                logger.warning(f"Overlapping key '{key}' detected. Existing value: {result[key]}, New value: {value}")
+                if type(result[key]) != type(value):
+                    logger.warning(
+                        f"Data type mismatch for key '{key}': {type(result[key])} vs {type(value)}."
+                    )
+            result[key] = value
+
+    return result
