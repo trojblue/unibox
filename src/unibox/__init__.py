f001f159086d627d50c02be64a01c312650eb9a0
yada <trojblue@gmail.com>
2025-02-20 17:40:39 +0000

    fix: missing type checks after update

--- src/unibox/__init__.py
+++ src/unibox/__init__.py
@@ -3,12 +3,25 @@
 unibox provides unified interface for common file operations
 """
 
+# unibox/__init__.py
 from __future__ import annotations
 
-__all__: list[str] = []
+__all__: list[str] = [
+    "concurrent_loads",
+    "gallery",
+    "label_gallery",
+    "loads",
+    "ls",
+    "peeks",
+    "saves",
+    "traverses",
+    "IMAGE_FILES",
+    "IMG_FILES",
+    "VIDEO_FILES",
+    "GLOBAL_TMP_DIR",
+    "UniLogger",
+]
 
-# main functions
-# IPython utilities (try except done in unibox.py)
 from .unibox import concurrent_loads, gallery, label_gallery, loads, ls, peeks, saves, traverses
 from .utils.constants import IMAGE_FILES, IMG_FILES, VIDEO_FILES
 from .utils.globals import GLOBAL_TMP_DIR
