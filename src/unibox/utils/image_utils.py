2af25725e0daca3192bb2bcdd409e55b491145e2
yada <trojblue@gmail.com>
2025-10-31 09:26:42 +0900

    refactor: update pyproject.toml for dynamic versioning and improve import order in multiple files

--- src/unibox/utils/image_utils.py
+++ src/unibox/utils/image_utils.py
@@ -1,10 +1,8 @@
 from PIL import Image, ImageDraw, ImageFont
-import numpy as np
 
 
 def concatenate_images_horizontally(images, max_height=1024) -> Image.Image:
-    """
-    Concatenates a list of PIL.Image objects horizontally, ensuring no image exceeds a specified maximum height.
+    """Concatenates a list of PIL.Image objects horizontally, ensuring no image exceeds a specified maximum height.
 
     :param images: List of PIL.Image objects.
     :param max_height: Maximum height for any image in the list. Images taller than this will be resized proportionally.
@@ -31,7 +29,7 @@ def concatenate_images_horizontally(images, max_height=1024) -> Image.Image:
     max_height = max(image.height for image in resized_images)
 
     # Create a new image with the appropriate dimensions
-    concatenated_image = Image.new('RGB', (total_width, max_height))
+    concatenated_image = Image.new("RGB", (total_width, max_height))
 
     # Paste each resized image into the new image
     x_offset = 0
@@ -48,10 +46,10 @@ def textsize(text, font):
     _, _, width, height = draw.textbbox((0, 0), text=text, font=font)
     return width, height
 
+
 def get_font_size(text, max_width, max_height, base_font, size_adjustment):
-    
-    if (len(text) < 7):
-        text = text + "a" * (7 - len(text)) # if text to short, add spaces to make it longer (to void oversized font)
+    if len(text) < 7:
+        text = text + "a" * (7 - len(text))  # if text to short, add spaces to make it longer (to void oversized font)
 
     font_size = 1
     while True:
@@ -61,54 +59,59 @@ def get_font_size(text, max_width, max_height, base_font, size_adjustment):
             break
         font_size += 1
 
-    if size_adjustment == 'default':
+    if size_adjustment == "default":
         font_size = font_size
-    elif size_adjustment == 'larger':
+    elif size_adjustment == "larger":
         font_size = min(font_size + 10, font_size * 2)  # Increase the font size
-    elif size_adjustment == 'smaller':
+    elif size_adjustment == "smaller":
         font_size = max(font_size - 10, font_size // 2)  # Decrease the font size
-    elif size_adjustment == 'smallest':
+    elif size_adjustment == "smallest":
         font_size = max(font_size - 30, 1)  # Decrease the font size
-    elif size_adjustment == 'largest':
+    elif size_adjustment == "largest":
         font_size = min(font_size + 30, font_size * 3)  # Increase the font size
 
     return base_font.font_variant(size=font_size - 1)
 
+
 def calculate_text_position(img_width, img_height, text_width, text_height, position, alignment, padding):
-    if position in ['top', 'bottom']:
-        text_y = padding / 2 if position == 'top' else img_height + padding / 2
-        if alignment == 'left':
+    if position in ["top", "bottom"]:
+        text_y = padding / 2 if position == "top" else img_height + padding / 2
+        if alignment == "left":
             text_x = padding / 2
-        elif alignment == 'right':
+        elif alignment == "right":
             text_x = img_width - text_width - padding / 2
         else:  # 'center'
             text_x = (img_width - text_width) / 2
     else:
         text_y = (img_height - text_height) / 2
-        text_x = padding / 2 if position == 'left' else img_width + padding / 2
-    
+        text_x = padding / 2 if position == "left" else img_width + padding / 2
+
     return text_x, text_y
+
 
 def create_new_image_with_space(pil_image, max_width, max_height, padding, position, text_height):
     img_width, img_height = pil_image.size
-    if position in ['top', 'bottom']:
-        new_image = Image.new('RGB', (img_width, img_height + text_height + padding), (255, 255, 255))
-        new_image.paste(pil_image, (0, text_height + padding) if position == 'top' else (0, 0))
+    if position in ["top", "bottom"]:
+        new_image = Image.new("RGB", (img_width, img_height + text_height + padding), (255, 255, 255))
+        new_image.paste(pil_image, (0, text_height + padding) if position == "top" else (0, 0))
     else:
-        new_image = Image.new('RGB', (img_width + max_width + padding, img_height), (255, 255, 255))
-        new_image.paste(pil_image, (max_width + padding, 0) if position == 'left' else (0, 0))
+        new_image = Image.new("RGB", (img_width + max_width + padding, img_height), (255, 255, 255))
+        new_image.paste(pil_image, (max_width + padding, 0) if position == "left" else (0, 0))
     return new_image
+
 
 def load_font():
     try:
         font_path = "/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf"  # Common path on many Linux systems
         return ImageFont.truetype(font_path, 1)  # Start with a small size to calculate the proper size
-    except IOError:
+    except OSError:
         return ImageFont.load_default()
 
-def add_annotation(pil_image: Image.Image, annotation: str, position: str = 'top', alignment: str = 'center', size: str = 'default'):
-    """
-    Adds a single annotation string as text to the top, left, right, or bottom of the image.
+
+def add_annotation(
+    pil_image: Image.Image, annotation: str, position: str = "top", alignment: str = "center", size: str = "default"
+):
+    """Adds a single annotation string as text to the top, left, right, or bottom of the image.
     Adjusts font size automatically according to actual image size to make it easy to look.
 
     Parameters:
@@ -122,14 +125,16 @@ def add_annotation(pil_image: Image.Image, annotation: str, position: str = 'top
     img_width, img_height = pil_image.size
 
     # Calculate the maximum width and height for the text
-    max_width = img_width if position in ['top', 'bottom'] else img_width // 5
-    max_height = img_height // 10 if position in ['top', 'bottom'] else img_height
+    max_width = img_width if position in ["top", "bottom"] else img_width // 5
+    max_height = img_height // 10 if position in ["top", "bottom"] else img_height
 
     font = get_font_size(annotation, max_width, max_height, font, size)
     text_width, text_height = textsize(annotation, font)
 
     padding = 10  # Add padding around the text
-    text_x, text_y = calculate_text_position(img_width, img_height, text_width, text_height, position, alignment, padding)
+    text_x, text_y = calculate_text_position(
+        img_width, img_height, text_width, text_height, position, alignment, padding
+    )
 
     new_image = create_new_image_with_space(pil_image, max_width, max_height, padding, position, text_height)
     draw = ImageDraw.Draw(new_image)
@@ -137,9 +142,9 @@ def add_annotation(pil_image: Image.Image, annotation: str, position: str = 'top
 
     return new_image
 
-def add_annotations(pil_image: Image.Image, annotations: list, position: str = 'top'):
-    """
-    Adds multiple annotation strings as text to the top, left, right, or bottom of the image.
+
+def add_annotations(pil_image: Image.Image, annotations: list, position: str = "top"):
+    """Adds multiple annotation strings as text to the top, left, right, or bottom of the image.
     Adjusts font size automatically according to actual image size to make it easy to look.
 
     Parameters:
@@ -151,8 +156,8 @@ def add_annotations(pil_image: Image.Image, annotations: list, position: str = '
     img_width, img_height = pil_image.size
 
     # Calculate the maximum width and height for the text
-    max_width = img_width if position in ['top', 'bottom'] else img_width // 5
-    max_height = img_height // 10 if position in ['top', 'bottom'] else img_height
+    max_width = img_width if position in ["top", "bottom"] else img_width // 5
+    max_height = img_height // 10 if position in ["top", "bottom"] else img_height
 
     padding = 10  # Add padding around the text
 
@@ -169,7 +174,9 @@ def add_annotations(pil_image: Image.Image, annotations: list, position: str = '
     for annotation, align, size in annotations:
         font = get_font_size(annotation, max_width, max_height, font, size)
         text_width, text_height = textsize(annotation, font)
-        text_x, text_y = calculate_text_position(img_width, img_height, text_width, max_text_height, position, align, padding)
+        text_x, text_y = calculate_text_position(
+            img_width, img_height, text_width, max_text_height, position, align, padding
+        )
         draw.text((text_x, text_y), annotation, fill="black", font=font)
 
     return new_image
